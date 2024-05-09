31f0f10a7f494ca7d59363a5085f71e6ec0d4532
arsh <arshayankhalid1@gmail.com>
2024-05-10 01:12:16 +0530

    'third'

--- vite-project/src/pages/Input.jsx
+++ vite-project/src/pages/Input.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 function Input() {
     const [calories, setCalories] = useState('');
@@ -7,8 +8,6 @@ function Input() {
     const [fat, setFat] = useState('');
     const [potassium, setPotassium] = useState('');
     const [result, setResult] = useState(null);
-
-
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -21,13 +20,15 @@ function Input() {
                 potassium: parseFloat(potassium),
             });
             const result = response.data;
+            console.log("itklsdafl");
             setResult(result);
 
 
-            console.log(response.data); // Handle the response from the server
+
         } catch (error) {
             console.error(error);
         }
+
     };
 
     const handleCaloriesChange = (event) => {
@@ -46,13 +47,49 @@ function Input() {
         setPotassium(event.target.value);
     };
 
+
+
     return (
         <>
+
             {result && (
-                <div>
-                    <h1>Optimal Cost</h1>
-                    {JSON.stringify(result)}
+                <div >
+                    <div className='grid grid-cols-2 gap-10 py-20'>
+                        <div className='flex gap-5 items-center flex-col'>
+                            <div className='text-6xl'>Optimal Cost</div>
+                            <h1 className='text-5xl'>Rs. {(result.optimal_cost / 10).toFixed(2)}</h1>
+
+
+                        </div>
+                        <div>
+                            <div className='text-3xl flex flex-col gap-5'>
+                                <div className='text-3xl bg-pink-800 text-white text-center inline p-2 rounded-xl '>What to Eat</div>
+                                <div>
+                                    {Object.entries(result.solution).map(([key, value]) => (
+                                        <li key={key}>
+                                            <span className="font-semibold capitalize">{key}:</span>{' '}
+                                            {(value * 10).toFixed(2)}g
+                                        </li>
+                                    ))}
+                                </div>
+
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+
+                    <div className='flex justify-center'>
+                        <div className='font-bold px-3 py-4 text-2xl bg-pink-800 text-white rounded-md inline-block'>
+                            <Link className="block text-center" to="/">Check Another</Link>
+                        </div>
+                    </div>
+
                 </div >
+
+
             )
             }
 
@@ -63,19 +100,19 @@ function Input() {
                         Enter the following details to get your optimal cost diet
                     </div>            <form onSubmit={handleSubmit} className="max-w-sm mx-auto">
                         <div className="mb-5">
-                            <label className="block mb-2 text-sm font-medium text-gray-900"> Calories</label>
+                            <label className="block mb-2 text-sm font-medium text-gray-900"> Calories(kcal)</label>
                             <input type="number" value={calories} onChange={handleCaloriesChange} id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " />
                         </div>
                         <div className="mb-5">
-                            <label className="block mb-2 text-sm font-medium text-gray-900"> Protein</label>
+                            <label className="block mb-2 text-sm font-medium text-gray-900"> Protein(g)</label>
                             <input type="number" value={protein} onChange={handleProteinChange} id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " />
                         </div>
                         <div className="mb-5">
-                            <label className="block mb-2 text-sm font-medium text-gray-900"> Fat</label>
+                            <label className="block mb-2 text-sm font-medium text-gray-900"> Fat(g)</label>
                             <input type="number" value={fat} onChange={handleFatChange} id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " />
                         </div>
                         <div className="mb-5">
-                            <label className="block mb-2 text-sm font-medium text-gray-900"> Potassium</label>
+                            <label className="block mb-2 text-sm font-medium text-gray-900"> Potassium(mg)</label>
                             <input type="number" value={potassium} onChange={handlePotassiumChange} id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " />
                         </div>
 
